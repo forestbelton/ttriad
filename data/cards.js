4823689ce392e9c6fe16c541f0e0d5b528fcf4cb
Forest Belton <fbelton@theore.tc>
2015-02-24 01:55:44 -0800

    Add card sample data

---
+++ data/cards.js
@@ -0,0 +1,10 @@
+var Cards = [
+    { name: "Bomb", stats: [3, 4, 3, 3] },
+    { name: "Coeurl", stats: [2, 5, 2, 5] },
+    { name: "Sabotender", stats: [4, 3, 3, 3] },
+    { name: "Mandragora", stats: [4, 2, 5, 3] },
+    { name: "Dodo", stats: [4, 2, 3, 4] },
+    { name: "Spriggan", stats: [2, 3, 4, 4] },
+    { name: "Ixal", stats: [6, 1, 4, 3] },
+    { name: "Blue Dragon", stats: [2, 2, 7, 7] }
+];
