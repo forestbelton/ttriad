00ea4672c2362d82ddaa29a68c67ffe8f9a64c00
Forest Belton <forest@homolo.gy>
2015-02-24 02:51:16 -0800

    Add LESS support and basic styles

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,15 @@ module.exports = {
         filename: 'bundle.js',
         path: __dirname + '/built'
     },
+    resolve: {
+        alias: {
+            less: __dirname + '/less'
+        }
+    },
     module: {
         loaders: [
-            { test: /.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+            { test: /.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' }
         ]
     }
 };
