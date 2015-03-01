d80fd0011f63ca446748ed51ad3e885bb6212520
Forest Belton <forest@homolo.gy>
2015-02-28 23:08:30 -0800

    Clean up Card component

--- components/Card.js
+++ components/Card.js
@@ -4,7 +4,16 @@ require('less/Card.less');
 var Card = React.createClass({
     propTypes: {
         color: React.PropTypes.oneOf(['red', 'blue']).isRequired,
-        inDeck: React.PropTypes.bool.isRequired
+        inDeck: React.PropTypes.bool.isRequired,
+        name: React.PropTypes.shape({
+            en: React.PropTypes.string
+        }).isRequired,
+        values: React.PropTypes.shape({
+            top:    React.PropTypes.number,
+            right:  React.PropTypes.number,
+            bottom: React.PropTypes.number,
+            left:   React.PropTypes.number
+        }).isRequired
     },
 
     onDragStart: function(ev) {
@@ -22,7 +31,8 @@ var Card = React.createClass({
         var cardImage = "data/" + ("000" + this.props.id).slice(-3) + ".png";
 
         return (
-            <div draggable={this.props.inDeck} onDragStart={this.onDragStart} className={cardClasses} alt={this.props.name.en}>
+            <div draggable={this.props.inDeck} onDragStart={this.onDragStart}
+                 className={cardClasses} alt={this.props.name.en}>
                 <img src={cardImage}/>
                 <div className="card-values">
                     <div className="card-top">{this.props.values.top}</div>
